255c51000f1265adb1ab2616b9d531e28e7c1a15
Nikhil Salgaonkar <nsalgaocar@gmail.com>
2026-03-02 23:23:33 +0530

    node: add direct fastify integration

--- packages/node/src/core.js
+++ packages/node/src/core.js
@@ -227,10 +227,15 @@ class Sentry {
     }
 
     _register(app) {
-        // Simple heuristic for now, can be improved
+        // NestJS
         if (app.getHttpAdapter && app.useGlobalInterceptors) {
             const NestIntegration = require('./integrations/nestjs');
             this.integration = new NestIntegration(this, app);
+        // Fastify
+        } else if (app.addHook && app.route && app.listen) {
+            const FastifyIntegration = require('./integrations/fastify');
+            this.integration = new FastifyIntegration(this, app);
+        // Express
         } else if (app.use && app.get && app.post) {
             const ExpressIntegration = require('./integrations/express');
             this.integration = new ExpressIntegration(this, app);
